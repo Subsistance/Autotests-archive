39b732b3164b2f29ba7e02eefcf9b185c4f87757
Subsistance <subsistancex@gmail.com>
2023-05-23 19:37:21 +0300

    Update 3.2 examples

--- Section3/assert strings examples.py
+++ Section3/assert strings examples.py
@@ -13,3 +13,6 @@ assert catalog_text == "Каталог", \
 # Решение задания
 def test_input_text(expected_result, actual_result):
     assert actual_result == expected_result, f"expected {expected_result}, got {actual_result}"
+    
+def test_substring(full_string, substring):
+    assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
